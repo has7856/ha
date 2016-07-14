185cbfff9b58038e6e1c1369a64c05fd85f5f483
junjw1 <junjw1@daum.net>
2016-07-15 01:49:19 +0900

    POST로 form의 데이터를 전송

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//미들웨어로서 bodyparser를 사용하기 위한 설정
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //정적 파일 서버를 설정하는 문장
 app.use(express.static(__dirname+'/public'));
 //public 디렉토리에 존재하는 파일을 찾기위해 routing table에 항목을 하나 추가한다.
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+app.get('/theteam',function(req, res, next){
+    res.sendFile(__dirname+'/public/theteam.html');
+})
+
+app.post('/great', function(req, res){
+    var name = req.body.teamname;
+    console.log(name);
+    res.end('yes');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+
+var server = app.listen(3000, function(){
+    console.log('server started on port 3000');
 });
