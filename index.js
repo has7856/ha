fdd8dff8518b0d0bc4f9c42de24f6057ea89d159
junjw1 <junjw1@daum.net>
2016-07-14 19:25:30 +0900

    페이지 나눈거 취소하고 싶어

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var app = express();
 
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 //정적 파일 서버를 설정하는 문장
 app.use(express.static(__dirname+'/public'));
 //public 디렉토리에 존재하는 파일을 찾기위해 routing table에 항목을 하나 추가한다.
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+
+var server = app.listen(3000, function(){
+    console.log('server started on port 3000');
+})
