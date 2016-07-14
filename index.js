03f5894499f6101649a131a52a611b9c904a750a
junjw1 <junjw1@daum.net>
2016-07-14 12:00:31 +0900

    디렉토리 구조 변경과 정적 파일 서버 설정

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+//정적 파일 서버를 설정하는 문장
+app.use(express.static(__dirname+'/public'));
+//public 디렉토리에 존재하는 파일을 찾기위해 routing table에 항목을 하나 추가한다.
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
