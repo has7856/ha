0ef05d90ae341217377faf8027f761ba4c77a0d0
junjw1 <junjw1@daum.net>
2016-07-14 19:16:36 +0900

    각 페이지 나누기

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname+'/public'));
 //public 디렉토리에 존재하는 파일을 찾기위해 routing table에 항목을 하나 추가한다.
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
